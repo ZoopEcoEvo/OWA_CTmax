923aff798363e0c4ddab416e8aad9478ddab94da
matt-sasaki <mcsasaki20@gmail.com>
2022-11-08 17:47:41 -0500

    Adding the setup guide script.

---
+++ Scripts/00_setup_guide.R
@@ -0,0 +1,19 @@
+# To generate the setup guide:
+# Update this value when you run the script by replacing the three letters inside the quotation marks
+user = "MCS" #First, middle, and last initial
+
+# Hit the "Source" button in the top right corner of this window
+
+#############################################
+### Do not alter anything below this line ###
+#############################################
+
+date = Sys.Date()
+lineages = c("AA", "AH", "HA", "HH")
+n = 10
+
+guide = data.frame(user, date, tube = c(1:n),
+                   lineage = sample(lineages, size = n, replace = T),
+                   replicate = NA)
+
+write.csv(guide, file = paste("setup-", Sys.Date(), ".csv", sep = ""))
