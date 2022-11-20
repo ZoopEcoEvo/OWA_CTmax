076d9bc8b77e38abea918fadc75a564d973623a7
Matthew Sasaki <mcsasaki20@gmail.com>
2022-11-19 21:57:31 -0500

    Adding another round of methodological validation - CTmax test tubes and holding setup.

--- Scripts/00_method_valid.R
+++
@@ -1,8 +0,0 @@
-library(tidyverse)
-
-tubes = read_csv(file = "Data/method_valid/tube_pH.csv")
-
-ggplot(tubes, aes(x = time, y = pH, group = tube, colour = temp, linetype = cover)) + 
-  facet_wrap(volume~.) +  
-  geom_line(size = 2) + 
-  theme_bw()
